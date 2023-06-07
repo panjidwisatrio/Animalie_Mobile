bf314ad98a194ea6f4031481a2f35539536e7a60
Irsal Lazuard Izzatillah <irsallazuard547@gmail.com>
2023-06-07 20:03:35 +0700

    fix: viewpager my profile issue

--- app/src/main/java/com/panji/animalie/ui/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/panji/animalie/ui/editprofile/EditProfileActivity.kt
@@ -1,11 +1,11 @@
 package com.panji.animalie.ui.editprofile
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.panji.animalie.R
 import com.panji.animalie.databinding.ActivityEditProfileBinding
-import com.panji.animalie.util.Constanta
 
 class EditProfileActivity : AppCompatActivity() {
 
@@ -16,14 +16,32 @@ class EditProfileActivity : AppCompatActivity() {
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //set top app bar
+        val appBar = binding.topAppBar
+        setSupportActionBar(appBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        appBar.setNavigationOnClickListener {
+            finish()
+        }
+
         //retrieve data from previous activity
+        binding.apply {
+            //set profile picture
+            val profilePict = intent.getStringExtra("EXTRA_AVATAR")
 
-        //set form text
-        binding.editFullName.setText(intent.getStringExtra("EXTRA_FULLNAME"))
-        binding.editUsername.setText(intent.getStringExtra("EXTRA_USERNAME"))
-        binding.editJob.setText(intent.getStringExtra("EXTRA_JOB"))
-        binding.editWorkplace.setText(intent.getStringExtra("EXTRA_WORKPLACE"))
-        binding.editEmail.setText(intent.getStringExtra("EXTRA_EMAIL"))
+            if (profilePict == null) {
+                profilePhoto.setImageResource(R.mipmap.profile_photo_round)
+            } else {
+                //TODO: retrieve from storage
+                profilePhoto.setImageResource(R.mipmap.profile_photo_round)
+            }
 
+            //set form text
+            editFullName.setText(intent.getStringExtra("EXTRA_FULLNAME"))
+            editUsername.setText(intent.getStringExtra("EXTRA_USERNAME"))
+            editJob.setText(intent.getStringExtra("EXTRA_JOB"))
+            editWorkplace.setText(intent.getStringExtra("EXTRA_WORKPLACE"))
+            editEmail.setText(intent.getStringExtra("EXTRA_EMAIL"))
+        }
     }
 }
